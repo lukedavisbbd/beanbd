15e71d6be3ab2de025c625ddb787d72835244f83
= <=>
2025-02-17 17:21:27 +0200

    add address, port outputs

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,11 @@
 output "beanbd_db_endpoint" {
   value = aws_db_instance.beanbd_instance.endpoint
 }
+
+output "beanbd_db_address" {
+  value = aws_db_instance.beanbd_instance.address
+}
+
+output "beanbd_db_port" {
+  value = aws_db_instance.beanbd_instance.port
+}
