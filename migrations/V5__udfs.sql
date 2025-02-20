c1900231ac41204f184d3341f52ba951068751fc
Ndzalama Mabasa <Ndzalama.Mabasa@bbd.co.za>
2025-02-20 09:46:49 +0200

    add a function to get number of coffee orders on a given time period of a given month and year

--- migrations/V5__udfs.sql
+++ migrations/V5__udfs.sql
@@ -1,13 +1,14 @@
-CREATE FUNCTION dbo.ufnGetOrdersOfTheDayInTimePeriod(@startTime TIME, @endTime TIME, @day DATE = NULL ) 
+CREATE FUNCTION dbo.ufnGetOrdersOfTheMonthInGivenTime(@startTime TIME, @endTime TIME, @year INT, @month INT ) 
 RETURNS int
 BEGIN
-	IF @day IS NULL
-        SET @day = CAST(GETDATE() AS DATE);
-
+	IF @year IS NULL
+        SET @year = YEAR(GETDATE());
+	IF @month IS NULL
+        SET @month = MONTH(GETDATE());
 	DECLARE @NumberOfOrders int;
 	SELECT @NumberOfOrders = COUNT(id)
 	FROM dbo.coffee_orders
-	WHERE (FORMAT(ordered_at, 'HH:mm:ss') BETWEEN '08:00:00' AND '11:59:59') AND (FORMAT(ordered_at, 'yyyy-MM-dd') = @day);
+	WHERE (FORMAT(ordered_at, 'HH:mm:ss') BETWEEN '08:00:00' AND '11:59:59') AND (YEAR(ordered_at) = @year) AND (MONTH(ordered_at) = @month);
 
 	RETURN @NumberOfOrders;
 END;
