611582cd02fb26bfc234681a6db08db34b11bde9
= <=>
2025-02-20 13:54:28 +0200

    Resolve Merge Issue

--- migrations/V5__udfs.sql
+++ migrations/V5__udfs.sql
@@ -1,4 +1,3 @@
-
 -- Function to get number of orders in a time period for a given date
 CREATE FUNCTION udfGetOrdersOfTheDayInTimePeriod(@startTime TIME, @endTime TIME, @day DATE = NULL ) 
 RETURNS int
@@ -8,7 +7,7 @@ BEGIN
 
 	DECLARE @NumberOfOrders int;
 	SELECT @NumberOfOrders = COUNT(id)
-	FROM dbo.coffee_orders
+	FROM coffee_orders
 	WHERE (FORMAT([ordered_at], 'HH:mm:ss') BETWEEN @startTime AND @endTime) AND (FORMAT([ordered_at], 'yyyy-MM-dd') = @day);
 
 	RETURN @NumberOfOrders;
@@ -25,36 +24,21 @@ BEGIN
         SET @month = MONTH(GETDATE());
 	DECLARE @NumberOfOrders int;
 	SELECT @NumberOfOrders = COUNT(id)
-	FROM dbo.coffee_orders
+	FROM coffee_orders
 	WHERE (FORMAT([ordered_at], 'HH:mm:ss') BETWEEN @startTime AND @endTime) AND (YEAR([ordered_at]) = @year) AND (MONTH([ordered_at]) = @month);
 
 	RETURN @NumberOfOrders;
 END;
 GO
 
--- Function to get number of orders in a time period for given year
-CREATE FUNCTION dbo.ufnGetOrdersOfTheYearInGivenTimePeriod(@startTime TIME, @endTime TIME, @year INT = NULL) 
-RETURNS int
-BEGIN
-	IF @year IS NULL
-        SET @year = YEAR(GETDATE());
-	DECLARE @NumberOfOrders int;
-	SELECT @NumberOfOrders = COUNT(id)
-	FROM dbo.coffee_orders
-	WHERE (FORMAT([ordered_at], 'HH:mm:ss') BETWEEN @startTime AND @endTime) AND (YEAR([ordered_at]) = @year)
-
-	RETURN @NumberOfOrders;
-END;
-GO
-
 -- Function to get the busiest period of the day between morning and afternoon
-CREATE FUNCTION dbo.ufnGetBusiestPeriodOfTheDay(@day DATE)
+CREATE FUNCTION udfGetBusiestPeriodOfTheDay(@day DATE)
 RETURNS TABLE
 RETURN
 (
     SELECT CONVERT(DATE, [ordered_at]) as OrderDate,
-	dbo.ufnGetOrdersOfTheDayInTimePeriod('08:00:00', '11:59:59', @day) as MorningOrders,
-	dbo.ufnGetOrdersOfTheDayInTimePeriod('12:00:00', '16:00:00', @day) as AfternoonOrders
+	udfGetOrdersOfTheDayInTimePeriod('08:00:00', '11:59:59', @day) as MorningOrders,
+	udfGetOrdersOfTheDayInTimePeriod('12:00:00', '16:00:00', @day) as AfternoonOrders
 FROM [coffee_orders]
 WHERE CONVERT(DATE, [ordered_at]) = @day
 GROUP BY CONVERT(DATE, [ordered_at])
