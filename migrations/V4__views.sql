d692eb5248fe64407bb3b4b4133c3bdcdd9623ab
= <=>
2025-02-21 09:07:53 +0200

    Fix Views

--- migrations/V4__views.sql
+++ migrations/V4__views.sql
@@ -71,3 +71,27 @@ AS
     FROM vStockOrders
     WHERE order_status='pending';
 GO
+
+CREATE VIEW vCoffeeOrdersForThePast30Days
+AS
+    SELECT CONVERT(DATE,[ordered_at]) as OrderDate, COUNT([coffee]) as NumberOfOrders
+    FROM coffee_orders
+    WHERE CONVERT(DATE,[ordered_at]) >= DATEADD(DAY, -30, GETDATE())
+    GROUP BY CONVERT(DATE,[ordered_at])
+GO
+
+
+-- view completed stock orders for each item in a month
+CREATE VIEW vCompletedStockOrdersForEachMonthByItem
+AS
+    SELECT s.name, so.status
+	  , YEAR(so.ordered_at) AS year
+      , MONTH(so.ordered_at) AS month, CONCAT(SUM(si.quantity), ' ', s.unit) AS quantity
+    FROM stock_order_items AS si
+        JOIN stock_orders AS so
+        ON (si.[order] = so.[id])
+        JOIN stock AS s
+        ON si.[stock_item] = s.id
+    WHERE so.status = 'completed'
+    GROUP BY s.name, YEAR(so.ordered_at), MONTH(so.ordered_at), so.status,  s.unit
+GO
--- migrations/V6__views.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW vCoffeeOrdersForThePast30Days
-AS
-    SELECT CONVERT(DATE,[ordered_at]) as OrderDate, COUNT([coffee]) as NumberOfOrders
-    FROM coffee_orders
-    WHERE CONVERT(DATE,[ordered_at]) >= DATEADD(DAY, -30, GETDATE())
-    GROUP BY CONVERT(DATE,[ordered_at])
-GO
